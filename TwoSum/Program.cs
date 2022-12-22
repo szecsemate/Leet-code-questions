ec1af70beecd3d11ddb5d654fd0a99f1eeb5ea28
szecsemate <kockagamer85@gmail.com>
2022-12-22 18:49:17 +0100

    Solved TwoSum

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Jackie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         int[] nums = new int[10];
+         int target = 12;   
+
+         for(int i = 0; i < nums.Length; i ++)
+         {
+            for(int k = i; k < nums.Length; k++)
+            {
+                if(nums[i] + nums[k] == target)
+                {
+                    System.Console.WriteLine(new int[] {i,k}); //return new int[] {i,k};
+                }
+            }
+         }
+            
+        }
+    }
+}
