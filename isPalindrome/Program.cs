4fd538b3714350f962384bb81a74d4c0878e502e
szecsemate <kockagamer85@gmail.com>
2023-06-21 15:28:07 +0200

    Finished IsPalindrome

---
+++ isPalindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class Solution {
+    public bool IsPalindrome(int x) {
+
+        string converted = Convert.ToString(x);
+        
+        
+
+        for(int i =0; i < converted.Length/2; i++)
+        {
+            int a = i +1;
+            if(converted[i]!=converted[^a])
+            {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+
+}
